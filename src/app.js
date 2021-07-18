aefba9ec22d0ba53c5aee59fc1725ce59b589f05
TransformativeBeing <83785114+TransformativeBeing@users.noreply.github.com>
2021-07-18 12:13:17 -0700

    updated map
    still need to add place search

--- src/app.js
+++ src/app.js
@@ -1,10 +1,11 @@
 // Initialize and add the map
 function initMap() {
-    // The location of Uluru 48.242387812111126, -122.35118074432776
+    // The location of Uluru location 48.242387812111126, -122.35118074432776
+    // Place ID = ChIJbyKm1mhchVQRP0y4pKMqKeo
     const uluru = { lat: 48.242387812111126, lng: -122.35118074432776 };
     // The map, centered at Uluru
     const map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 4,
+        zoom: 11,
         center: uluru,
     });
     // The marker, positioned at Uluru
@@ -13,5 +14,3 @@ function initMap() {
         map: map,
     });
 }
-
-initMap();
